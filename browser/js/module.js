3271674b542c3851079e2bf319c657fe45f290b7
Elliott McNary <El_Is_Based@MacBook-Pro.local>
2016-07-07 16:43:27 -0400

    on artists now

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,3 +1,11 @@
-'use strict';
+// 'use strict';
 
-var juke = angular.module('juke', []);
+var juke = angular.module('juke', ['ui.router']);
+
+juke.config(function($stateProvider){
+  $stateProvider.state('allAlbums', {
+    url: '/albums',
+    templateUrl: '/js/templates/albumsTemplate.html',
+    controller: 'AlbumsCtrl'
+  })
+})
--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -11,10 +11,12 @@ module.exports = function (app) {
   var npmPath = path.join(root, './node_modules');
   var publicPath = path.join(root, './public');
   var browserPath = path.join(root, './browser');
+  var uiRouter = path.join(root, './node_modules/angular-ui-router');
 
   app.use(favicon(app.getValue('faviconPath')));
   app.use(express.static(npmPath));
   app.use(express.static(publicPath));
   app.use(express.static(browserPath));
+    app.use(express.static(uiRouter));
 
 };
